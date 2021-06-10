aca15e29c236b7d0ce42bcc25b13a15d88f052e0
Margaux7 <margaux7222@gmail.com>
2021-06-10 14:54:05 +0800

    chore: add version after output filename

--- webpack/browser.js
+++ webpack/browser.js
@@ -1,21 +1,19 @@
 const merge = require('webpack-merge');
 const config = require('./common');
+const { version } = require('@package/../../package.json');
 
 const browser = merge(config, {
   mode: 'none',
   output: {
-    filename: 'inspirecloud-browser.js'
+    filename: `inspirecloud-browser-${version}.js`
   }
 });
 
 const browserMin = merge(config, {
   mode: 'production',
   output: {
-    filename: 'inspirecloud-browser.min.js'
+    filename: `inspirecloud-browser-${version}.min.js`
   }
 });
 
-module.exports = [
-  browser,
-  browserMin
-];
+module.exports = [browser, browserMin];
