6e49f9f5754192eff5dc216db5d6df689c1ffd1c
Margaux7 <Margaux7222@gmail.com>
2021-08-05 16:04:56 +0800

    chore: add action update entry file version suffix (#6)
    * chore: add action update entry file version suffix
    
    * chore: use min file as entry file

---
+++ scripts/updateEntryFile.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const version = require('../package.json').version;
+
+function updateEntryFile() {
+  let body = fs.readFileSync(`${process.cwd()}/package.json`, 'utf-8');
+  body = body.replace(
+    /\.\/dist\/inspirecloud-.*\.js/g,
+    `./dist/inspirecloud.min-${version}.js`
+  );
+  fs.writeFileSync(`${process.cwd()}/package.json`, body);
+}
+
+updateEntryFile();
