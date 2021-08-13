d64acf940df1fc3ae2eb296197222cc6184c97d1
Month77 <2587423053@qq.com>
2021-08-13 18:53:16 +0800

    chore: update action file

--- scripts/updateEntryFile.js
+++ scripts/updateEntryFile.js
@@ -4,7 +4,7 @@ const version = require('../package.json').version;
 function updateEntryFile() {
   let body = fs.readFileSync(`${process.cwd()}/package.json`, 'utf-8');
   body = body.replace(
-    /\.\/dist\/inspirecloud-.*\.js/g,
+    /\.\/dist\/inspirecloud.*\.js/g,
     `./dist/inspirecloud.min-${version}.js`
   );
   fs.writeFileSync(`${process.cwd()}/package.json`, body);
